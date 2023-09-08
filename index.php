44b5c49b2fa3eb185bb6c5f9482f569f747ae951
OKyoonho <dbsgh037@naver.com>
2023-09-08 18:18:02 +0900

    9/8

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
     <?php
     $title=array('Farm Farm','건강한 먹거리');
     include('./page/module/header.php');
-    // include('./page/module/mysql.php');
     include('./page/module/carusel.php');
         ?>
         </div>
